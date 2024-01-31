b28a67c50e433db38511ad56bce56f07dff5ad30
Andreas Hellberg <andreas.hellberg@frankdigital.se>
2024-01-31 16:19:52 +0100

    adds hardcoded links to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,22 @@ import Head from "next/head"
 import styled from "styled-components"
 
 function Home() {
-  return <div>INDEX</div>
+  return (
+    <StyledBody>
+      <h2>songs</h2>
+      <a href="/down-to-the-river">Down to the river</a>
+
+      <a href="/hallelujah">Hallelujah</a>
+    </StyledBody>
+  )
 }
 
-const StyledBody = styled.div``
+const StyledBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  a {
+    margin-bottom: 1rem;
+  }
+`
 export default Home
